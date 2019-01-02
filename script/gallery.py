7ffe28ba6ea6980124b8eb87fc0d6566ebe4b346
Masahiko Aida <masabu@Masahikos-Mac-mini.local>
2019-01-01 22:00:17 -0600

    changing library from pip to rawpy

--- script/gallery.py
+++ script/gallery.py
@@ -1,303 +1,345 @@
-from PIL import Image
-import ExifTags
+#from PIL import Image
+import rawpy
+import imageio
+#import ExifTags
 import glob
 import argparse
-import os
-import errno
-import markup
-from markup import oneliner as e
-import json
-import random
+# import os
+# import errno
+# import markup
+# from markup import oneliner as e
+# import json
+# import random
 
-## process single image
-def CreateSmallerImages(folder, filename, size, update):
+'''
+# outline of the program
 
-    ## make thumbnail folder
-    try:
-        os.mkdir(folder + '/thumbs')
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
+inputs
+(1) input dir where raw files are saved
+(2) output dir where jpg files will be saved
+(3) hard code - raw file accepted
 
-    # store EXIF info as list of dictionary
-    ExifDict = {}
-
-    img = Image.open(folder + '/' + filename)
-    exif_data = img._getexif()
-
-    if exif_data is not None:
-
-        #print exif_data
-        for key, value in sorted(exif_data.items()):
-
-            keyname = ExifTags.TAGS.get(key)
-            
-            if keyname == 'Orientation':
-
-                if value == 3: img = img.rotate(180)
-                if value == 6: img = img.rotate(270)
-                if value == 8: img = img.rotate(90)
-
-        # store meta data for index
-
-        try:        
-            ExifDict['DateTime']        = exif_data[306]
-        except:
-            ExifDict['ExposureTime']    = 'Missing'
-
-        try:
-            ExifDict['ExposureTime']    = exif_data[33434]
-        except:
-            ExifDict['ExposureTime']    = 0
-        try:
-            ExifDict['FNumbe']          = exif_data[33437]
-        except:
-            ExifDict['FNumbe']          = (0,0)
-        try:
-            ExifDict['ISOSpeedRatings'] = exif_data[34855]
-        except:
-            ExifDict['ISOSpeedRatings'] = 0
-        try:
-            ExifDict['Model']           = exif_data[272]
-        except:
-            ExifDict['Model']           = 'Missing'
-        try:
-            ExifDict['Orientation']     = exif_data[274]
-        except:
-            pass
-        try:
-            ExifDict['ApertureValue']   = exif_data[37378]
-        except:
-            pass
-
-    else:
-        print 'No Exit data, will not be rotated'
-
-    ## export thumnail file, clean up buffer otherwise it can corrupt!!
-
-    if update == 'FALSE':
-
-        out_file = open( folder + '/thumbs/' + filename, 'wb' )
-        img.thumbnail(size, Image.ANTIALIAS)
-        quality_val = 90
-        img.save( out_file, 'JPEG' , quality=quality_val)  # Must specify desired format here
-        out_file.flush()
-        out_file.close()
-    else:
-        pass
-
-    return ExifDict
-
-
-# from stackoverflow :)
-def insensitive_glob(pattern):
-    def either(c):
-        return '[%s%s]'%(c.lower(),c.upper()) if c.isalpha() else c
-    return glob.glob(''.join(map(either,pattern)))
-
-
-def MakeHTMLS(filenames, folder, EXIFs, segment, MaxSegment):
-
-    try:
-        import markup
-    except:
-        print __doc__
-        sys.exit( 1 )
-
-    ## initialization 
-    page = markup.page( )
-    page.init( title="Photo Gallery",
-    css=( '../one.css'), 
-    header="Produced by Pig Thumbnail Generator", 
-    footer="" )
-
-    ## define lightbox
-    page.link(href="../lightbox/css/lightbox.css",  rel="stylesheet")
-    page.script("" , src="../lightbox/js/jquery-1.11.0.min.js")
-    page.script( "", src="../lightbox/js/lightbox.js")
-
-    ## center everything
-    #page.div(class_ = "center")
-
-    ## test
-    page.H1("Photo Gallery " + folder)
-
-    page.h2(e.a( 'Go Back to the main index' , href = '../index.html'))
-
-    for s in range(MaxSegment):
-
-        if s == 0:
-            page.h2(e.a( 'Page' + str(s) , href = 'index.html'))
-        else:
-            page.h2(e.a( 'Page' + str(s) , href = 'index' + str(s) + '.html'))
-   
-    for i in range(len(filenames)):
-        ## start of gallery area
-        page.div(class_ = "gallery")
-        page.div(class_ = "row")
-
-        page.a( e.img( src= 'thumbs/' + filenames[i] , height=280 ), data_title=filenames[i] + ';' +EXIFs[filenames[i]], href = 'thumbs/' + filenames[i], class_='image-link', data_lightbox="set" )
-        page.div(e.a(filenames[i],  href = filenames[i]),class_ = "desc")
-
-        page.div.close( ) # close image row
-        page.div.close( ) # close gallery
-
-    if segment == 0 :
-        outfile = 'index.html'
-    else :
-        outfile = 'index' + str(segment) + '.html'
-
-    print 's=',segment, 'saving', outfile
-
-    file = open( folder + '/' + outfile , 'w')
-    print >>file, page
-    file.flush()
-    file.close()  
+(4) recurively go after each file in subfolder
+(5) convert raw (DNC, CR2 or whatever - but specify) files into jpeg
+(6) create html file 
+'''
 
 def main(folder, update):
 
     print 'process images inside', folder
 
-    # create index.html inside specific image folder
-    # container to store image names
+#     # create index.html inside specific image folder
+#     # container to store image names
  
-    htmls = []
-    filetypes = ('*.jpg', '*.jpeg')
-    filenames = []
+#     htmls = []
+#     filetypes = ('*.jpg', '*.jpeg')
+#     filenames = []
 
-    ## obtain file names
-    for t in filetypes:
-        for fullpath in insensitive_glob(folder + '/'  + t):
+#     ## obtain file names
+#     for t in filetypes:
+#         for fullpath in insensitive_glob(folder + '/'  + t):
 
-            #print 'match pattern', t, 'file is matched is ', fullpath
-            # remove path
-            fname = fullpath.replace(folder + '/', "")
-            filenames.append(fname)
+#             #print 'match pattern', t, 'file is matched is ', fullpath
+#             # remove path
+#             fname = fullpath.replace(folder + '/', "")
+#             filenames.append(fname)
 
-    print '\n', len(filenames), 'images found in ', folder, '\n'
+#     print '\n', len(filenames), 'images found in ', folder, '\n'
 
-    EXIFs = {}
 
-    ## make thumbnails
-    j = 0.0
-    for i in filenames:
 
-        ratio = j / len(filenames) * 100
-        print folder, ratio, "% completed"
+# ## process single image
+# def CreateSmallerImages(folder, filename, size, update):
 
-        Exif  = CreateSmallerImages(folder, i, (1200,1200), update)
-        EXIFs[i] = json.dumps(Exif)
+#     ## make thumbnail folder
+#     try:
+#         os.mkdir(folder + '/thumbs')
+#     except OSError as exception:
+#         if exception.errno != errno.EEXIST:
+#             raise
 
-        j +=1
+#     # store EXIF info as list of dictionary
+#     ExifDict = {}
 
-    ## if number of images are large, split into multiple htmls    
-    segment = len(filenames) / 50
+#     img = Image.open(folder + '/' + filename)
+#     exif_data = img._getexif()
 
-    if segment == 0:
+#     if exif_data is not None:
 
-        MakeHTMLS(filenames, folder, EXIFs, 0,0)
+#         #print exif_data
+#         for key, value in sorted(exif_data.items()):
 
-    for s in range(segment):
+#             keyname = ExifTags.TAGS.get(key)
+            
+#             if keyname == 'Orientation':
 
-        beg = s * 50
-        end = (s+1)*50-1
+#                 if value == 3: img = img.rotate(180)
+#                 if value == 6: img = img.rotate(270)
+#                 if value == 8: img = img.rotate(90)
 
-        if s  < segment -1 :
-            print s, segment -1 , beg, end
-            MakeHTMLS(filenames[beg:end], folder, EXIFs, s,segment)
-        else:
-            print s, segment - 1, beg, "end"
-            MakeHTMLS(filenames[beg:], folder, EXIFs, s, segment)
+#         # store meta data for index
 
-    return filenames
+#         try:        
+#             ExifDict['DateTime']        = exif_data[306]
+#         except:
+#             ExifDict['ExposureTime']    = 'Missing'
+
+#         try:
+#             ExifDict['ExposureTime']    = exif_data[33434]
+#         except:
+#             ExifDict['ExposureTime']    = 0
+#         try:
+#             ExifDict['FNumbe']          = exif_data[33437]
+#         except:
+#             ExifDict['FNumbe']          = (0,0)
+#         try:
+#             ExifDict['ISOSpeedRatings'] = exif_data[34855]
+#         except:
+#             ExifDict['ISOSpeedRatings'] = 0
+#         try:
+#             ExifDict['Model']           = exif_data[272]
+#         except:
+#             ExifDict['Model']           = 'Missing'
+#         try:
+#             ExifDict['Orientation']     = exif_data[274]
+#         except:
+#             pass
+#         try:
+#             ExifDict['ApertureValue']   = exif_data[37378]
+#         except:
+#             pass
+
+#     else:
+#         print 'No Exit data, will not be rotated'
+
+#     ## export thumnail file, clean up buffer otherwise it can corrupt!!
+
+#     if update == 'FALSE':
+
+#         out_file = open( folder + '/thumbs/' + filename, 'wb' )
+#         img.thumbnail(size, Image.ANTIALIAS)
+#         quality_val = 90
+#         img.save( out_file, 'JPEG' , quality=quality_val)  # Must specify desired format here
+#         out_file.flush()
+#         out_file.close()
+#     else:
+#         pass
+
+#     return ExifDict
+
+
+# # from stackoverflow :)
+# def insensitive_glob(pattern):
+#     def either(c):
+#         return '[%s%s]'%(c.lower(),c.upper()) if c.isalpha() else c
+#     return glob.glob(''.join(map(either,pattern)))
+
+
+# def MakeHTMLS(filenames, folder, EXIFs, segment, MaxSegment):
+
+#     try:
+#         import markup
+#     except:
+#         print __doc__
+#         sys.exit( 1 )
+
+#     ## initialization 
+#     page = markup.page( )
+#     page.init( title="Photo Gallery",
+#     css=( '../one.css'), 
+#     header="Produced by Pig Thumbnail Generator", 
+#     footer="" )
+
+#     ## define lightbox
+#     page.link(href="../lightbox/css/lightbox.css",  rel="stylesheet")
+#     page.script("" , src="../lightbox/js/jquery-1.11.0.min.js")
+#     page.script( "", src="../lightbox/js/lightbox.js")
+
+#     ## center everything
+#     #page.div(class_ = "center")
+
+#     ## test
+#     page.H1("Photo Gallery " + folder)
+
+#     page.h2(e.a( 'Go Back to the main index' , href = '../index.html'))
+
+#     for s in range(MaxSegment):
+
+#         if s == 0:
+#             page.h2(e.a( 'Page' + str(s) , href = 'index.html'))
+#         else:
+#             page.h2(e.a( 'Page' + str(s) , href = 'index' + str(s) + '.html'))
+   
+#     for i in range(len(filenames)):
+#         ## start of gallery area
+#         page.div(class_ = "gallery")
+#         page.div(class_ = "row")
+
+#         page.a( e.img( src= 'thumbs/' + filenames[i] , height=280 ), data_title=filenames[i] + ';' +EXIFs[filenames[i]], href = 'thumbs/' + filenames[i], class_='image-link', data_lightbox="set" )
+#         page.div(e.a(filenames[i],  href = filenames[i]),class_ = "desc")
+
+#         page.div.close( ) # close image row
+#         page.div.close( ) # close gallery
+
+#     if segment == 0 :
+#         outfile = 'index.html'
+#     else :
+#         outfile = 'index' + str(segment) + '.html'
+
+#     print 's=',segment, 'saving', outfile
+
+#     file = open( folder + '/' + outfile , 'w')
+#     print >>file, page
+#     file.flush()
+#     file.close()  
+
+# def main(folder, update):
+
+#     print 'process images inside', folder
+
+#     # create index.html inside specific image folder
+#     # container to store image names
+ 
+#     htmls = []
+#     filetypes = ('*.jpg', '*.jpeg')
+#     filenames = []
+
+#     ## obtain file names
+#     for t in filetypes:
+#         for fullpath in insensitive_glob(folder + '/'  + t):
+
+#             #print 'match pattern', t, 'file is matched is ', fullpath
+#             # remove path
+#             fname = fullpath.replace(folder + '/', "")
+#             filenames.append(fname)
+
+#     print '\n', len(filenames), 'images found in ', folder, '\n'
+
+#     EXIFs = {}
+
+#     ## make thumbnails
+#     j = 0.0
+#     for i in filenames:
+
+#         ratio = j / len(filenames) * 100
+#         print folder, ratio, "% completed"
+
+#         Exif  = CreateSmallerImages(folder, i, (1200,1200), update)
+#         EXIFs[i] = json.dumps(Exif)
+
+#         j +=1
+
+#     ## if number of images are large, split into multiple htmls    
+#     segment = len(filenames) / 50
+
+#     if segment == 0:
+
+#         MakeHTMLS(filenames, folder, EXIFs, 0,0)
+
+#     for s in range(segment):
+
+#         beg = s * 50
+#         end = (s+1)*50-1
+
+#         if s  < segment -1 :
+#             print s, segment -1 , beg, end
+#             MakeHTMLS(filenames[beg:end], folder, EXIFs, s,segment)
+#         else:
+#             print s, segment - 1, beg, "end"
+#             MakeHTMLS(filenames[beg:], folder, EXIFs, s, segment)
+
+#     return filenames
         
 
-def SuperIndex(folder, update, NSample):
+# def SuperIndex(folder, update, NSample):
 
-    # glob folder names
-    # assumes ISO-date  20xx-xx-xx 
+#     # glob folder names
+#     # assumes ISO-date  20xx-xx-xx 
 
-    print '\n'
-    print '*********************************************************************'
-    print "This program assumes ISO-date format directories such as 2015-01-05"
-    print '*********************************************************************\n'
+#     print '\n'
+#     print '*********************************************************************'
+#     print "This program assumes ISO-date format directories such as 2015-01-05"
+#     print '*********************************************************************\n'
 
-    Directories = []
-    CoverPhotos = []
+#     Directories = []
+#     CoverPhotos = []
 
-    for directory in insensitive_glob('????-??-??'):
+#     for directory in insensitive_glob('????-??-??'):
 
-        filenames = main(directory , update)
-        Directories.append(directory + '/index.html')
+#         filenames = main(directory , update)
+#         Directories.append(directory + '/index.html')
 
-        cover = []
-        # select 5 images
-        for i in range(NSample):
+#         cover = []
+#         # select 5 images
+#         for i in range(NSample):
 
-            rand = random.randint(0, len(filenames)-1)
-            #print i, len(filenames), rand
-            cover.append(filenames[rand])
+#             rand = random.randint(0, len(filenames)-1)
+#             #print i, len(filenames), rand
+#             cover.append(filenames[rand])
         
-        CoverPhotos.append(cover)
+#         CoverPhotos.append(cover)
 
-    ## create super index file
-    ## initialization 
+#     ## create super index file
+#     ## initialization 
 
-    page = markup.page( )
-    page.init( title="Photo Gallery",
-    css=( '../one.css'), 
-    header="", 
-    footer="Produced by Pig Thumbnail Generator" )
+#     page = markup.page( )
+#     page.init( title="Photo Gallery",
+#     css=( '../one.css'), 
+#     header="", 
+#     footer="Produced by Pig Thumbnail Generator" )
 
-    page.H1("Photo Gallery")
+#     page.H1("Photo Gallery")
 
-    ## take random sample of photos
-    ## to create cover
+#     ## take random sample of photos
+#     ## to create cover
 
-    for i in range(len(Directories)):
+#     for i in range(len(Directories)):
 
-        page.p(Directories[i].replace("/index.html",""))
+#         page.p(Directories[i].replace("/index.html",""))
 
-        for j in range(NSample):
-            page.a(e.img(height=160, src= Directories[i].replace("index.html","") + 'thumbs/' + CoverPhotos[i][j]  ) ,href = Directories[i])
-        page.hr()
+#         for j in range(NSample):
+#             page.a(e.img(height=160, src= Directories[i].replace("index.html","") + 'thumbs/' + CoverPhotos[i][j]  ) ,href = Directories[i])
+#         page.hr()
 
-    file = open( "index.html" , 'w')
-    print >>file, page
-    file.flush()
-    file.close()  
+#     file = open( "index.html" , 'w')
+#     print >>file, page
+#     file.flush()
+#     file.close()  
 
 
-##########################################################################
-##########################################################################
-##########################################################################
+# ##########################################################################
+# ##########################################################################
+# ##########################################################################
 
 if __name__ == '__main__':
 
     '''
-    the program can scan directory and update all the images recursively
-
+    The program can scan directory and update all the images recursively
     two options:
 
     (1) full update -- update jpeg thumbnails and everything
     (2) update htmls only
-
     '''
 
-    parser=argparse.ArgumentParser(description='Create thumbnail keeping original file names')
-    parser.add_argument('--folder' , default='empty_folder', type=str , help='specify image folder name')
-    parser.add_argument('--update', default='FALSE' ,  help='TRUE = full update, default is html only', type = str)
-    parser.add_argument('--Nsample', default=10 ,  help='Number of sample images', type = int)
+    parser=argparse.ArgumentParser(description='Create jpeg thumbnail from raw image files')
+    parser.add_argument('-f' , default='empty_folder', type=str , help='specify input folder name')
+    parser.add_argument('-o' , default='empty_folder', type=str , help='specify output folder name')
+
+
+    #parser.add_argument('--update', default='FALSE' ,  help='TRUE = full update, default is html only', type = str)
+    #parser.add_argument('--Nsample', default=10 ,  help='Number of sample images', type = int)
  
 
     args=parser.parse_args()
 
-    if args.folder != 'empty_folder':
+    if args.f != 'empty_folder':
         print "update folder"
-        main(args.folder, args.update)
-    elif args.folder == 'empty_folder':
-        print "update everything"
-        SuperIndex(args.folder, args.update, args.Nsample)
+        main(args.f)
+    elif args.f == 'empty_folder':
+        print "Please specify folder!!"
+#elif args.folder == 'empty_folder':
+#    print "update everything"
+#         SuperIndex(args.folder, args.update, args.Nsample)
 
 
 
